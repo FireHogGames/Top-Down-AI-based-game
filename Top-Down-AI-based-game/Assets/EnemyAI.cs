c669e240bd22b65a5c96de9d139fc92df5278ee5
FireHogGames <hidde01@live.nl>
2017-09-10 19:04:24 +0200

    Update 1

---
+++ Top-Down-AI-based-game/Assets/EnemyAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyAI : MonoBehaviour {
+
+    public Transform target;
+    private NavMeshAgent aiAgent;
+
+    private void Start()
+    {
+        aiAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        aiAgent.SetDestination(target.position);
+    }
+}
---
+++ Top-Down-AI-based-game/Assets/UnitAI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class UnitAI : MonoBehaviour
+{
+    private NavMeshAgent aiAgent;
+
+    private Camera cam;
+
+    private void Start()
+    {
+        aiAgent = GetComponent<NavMeshAgent>();
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit _hit;
+
+            if (Physics.Raycast(ray, out _hit))
+            {
+                aiAgent.SetDestination(_hit.point);
+            }
+        }
+    }
+}
